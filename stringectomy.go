20e9630f24a35d76f364085faff04541b304119c
Michael Whatcott <mike@smarty.com>
2025-08-05 17:10:41 -0600

    Export underlying bytes. It's ok.

--- stringectomy.go
+++ stringectomy.go
@@ -3,62 +3,78 @@ package surgeon
 import "bytes"
 
 // Stringectomy facilitates trimming away parts of a text that the caller deems superfluous.
-type Stringectomy struct{ b []byte }
-
-func NewStringectomy(b []byte) *Stringectomy { return &Stringectomy{b: b} }
+type Stringectomy struct{ Text []byte }
 
 func clamp(lo, n, hi int) int {
 	return max(lo, min(n, hi))
 }
 
 func (this *Stringectomy) update(b []byte) (modified bool) {
-	if bytes.Equal(this.b, b) {
+	if bytes.Equal(this.Text, b) {
 		return false
 	}
-	this.b = b
+	this.Text = b
 	return true
 }
 
-func (this *Stringectomy) Bytes() []byte { return this.b }
+// Contains checks whether []byte(s) is found in the underlying bytes.
+func (this *Stringectomy) Contains(s string) bool {
+	return bytes.Contains(this.Text, []byte(s))
+}
 
+// Excise removes the first n occurrences of sub from the underlying bytes.
 func (this *Stringectomy) Excise(sub string, n int) bool {
-	return this.update(bytes.Replace(this.b, []byte(sub), []byte(""), n))
-}
-func (this *Stringectomy) ExciseAll(sub string) bool {
-	return this.update(bytes.ReplaceAll(this.b, []byte(sub), []byte("")))
+	return this.update(bytes.Replace(this.Text, []byte(sub), []byte(""), n))
 }
 
+// ExciseAll removes all occurrences of sub from the underlying bytes.
+func (this *Stringectomy) ExciseAll(sub string) bool {
+	return this.update(bytes.ReplaceAll(this.Text, []byte(sub), []byte("")))
+}
+
+// CutAfter cuts the string at the occurrence of s and discards all but what came before.
 func (this *Stringectomy) CutAfter(s string) bool {
-	before, _, _ := bytes.Cut(this.b, []byte(s))
+	before, _, _ := bytes.Cut(this.Text, []byte(s))
 	return this.update(before)
 }
+
+// CutBefore cuts the string at the occurrence of s and discards all but what came after.
 func (this *Stringectomy) CutBefore(s string) bool {
-	_, after, ok := bytes.Cut(this.b, []byte(s))
+	_, after, ok := bytes.Cut(this.Text, []byte(s))
 	if !ok {
 		return false
 	}
 	return this.update(after)
 }
 
+// DiscardNLeft discards up to n bytes from the beginning of the underlying bytes or until none remain.
 func (this *Stringectomy) DiscardNLeft(n int) bool {
-	return this.update(this.b[clamp(0, n, len(this.b)):])
+	return this.update(this.Text[clamp(0, n, len(this.Text)):])
 }
+
+// DiscardNRight discards up to n bytes from the
 func (this *Stringectomy) DiscardNRight(n int) bool {
-	return this.update(this.b[:len(this.b)-clamp(0, n, len(this.b))])
+	return this.update(this.Text[:len(this.Text)-clamp(0, n, len(this.Text))])
 }
 
+// TrimLeft trims all characters in set from the beginning of the underlying bytes.
 func (this *Stringectomy) TrimLeft(set string) bool {
-	return this.update(bytes.TrimLeft(this.b, set))
-}
-func (this *Stringectomy) TrimRight(set string) bool {
-	return this.update(bytes.TrimRight(this.b, set))
+	return this.update(bytes.TrimLeft(this.Text, set))
 }
 
+// TrimRight trims all characters in set from the end of the underlying bytes.
+func (this *Stringectomy) TrimRight(set string) bool {
+	return this.update(bytes.TrimRight(this.Text, set))
+}
+
+// CutPrefix removes the prefix (if present) from the underlying bytes.
 func (this *Stringectomy) CutPrefix(prefix string) (ok bool) {
-	this.b, ok = bytes.CutPrefix(this.b, []byte(prefix))
+	this.Text, ok = bytes.CutPrefix(this.Text, []byte(prefix))
 	return ok
 }
+
+// CutSuffix removes the suffix (if present) from the underlying bytes.
 func (this *Stringectomy) CutSuffix(suffix string) (ok bool) {
-	this.b, ok = bytes.CutSuffix(this.b, []byte(suffix))
+	this.Text, ok = bytes.CutSuffix(this.Text, []byte(suffix))
 	return ok
 }
--- stringectomy_test.go
+++ stringectomy_test.go
@@ -7,48 +7,51 @@ import (
 )
 
 func Test(t *testing.T) {
-	operation := NewStringectomy([]byte("The quick brown fox jumps over the lazy dog."))
+	operation := &Stringectomy{Text: []byte("The quick brown fox jumps over the lazy dog.")}
+
+	should.So(t, operation.Contains("jumps over"), should.BeTrue)
+	should.So(t, operation.Contains("JUMPS OVER"), should.BeFalse)
 
 	should.So(t, operation.CutPrefix("The"), should.BeTrue)
 	should.So(t, operation.CutPrefix("The"), should.BeFalse)
-	should.So(t, string(operation.Bytes()), should.Equal, " quick brown fox jumps over the lazy dog.")
+	should.So(t, string(operation.Text), should.Equal, " quick brown fox jumps over the lazy dog.")
 
 	should.So(t, operation.DiscardNLeft(1), should.BeTrue)
-	should.So(t, string(operation.Bytes()), should.Equal, "quick brown fox jumps over the lazy dog.")
+	should.So(t, string(operation.Text), should.Equal, "quick brown fox jumps over the lazy dog.")
 
 	should.So(t, operation.TrimRight("."), should.BeTrue)
 	should.So(t, operation.TrimRight("."), should.BeFalse)
-	should.So(t, string(operation.Bytes()), should.Equal, "quick brown fox jumps over the lazy dog")
+	should.So(t, string(operation.Text), should.Equal, "quick brown fox jumps over the lazy dog")
 
 	should.So(t, operation.ExciseAll("o"), should.BeTrue)
 	should.So(t, operation.ExciseAll("o"), should.BeFalse)
-	should.So(t, string(operation.Bytes()), should.Equal, "quick brwn fx jumps ver the lazy dg")
+	should.So(t, string(operation.Text), should.Equal, "quick brwn fx jumps ver the lazy dg")
 
 	should.So(t, operation.DiscardNRight(1), should.BeTrue)
-	should.So(t, string(operation.Bytes()), should.Equal, "quick brwn fx jumps ver the lazy d")
+	should.So(t, string(operation.Text), should.Equal, "quick brwn fx jumps ver the lazy d")
 
 	should.So(t, operation.Excise("the ", 1), should.BeTrue)
 	should.So(t, operation.Excise("the ", 1), should.BeFalse)
-	should.So(t, string(operation.Bytes()), should.Equal, "quick brwn fx jumps ver lazy d")
+	should.So(t, string(operation.Text), should.Equal, "quick brwn fx jumps ver lazy d")
 
 	should.So(t, operation.TrimLeft("kciuq"), should.BeTrue)
 	should.So(t, operation.TrimLeft("kciuq"), should.BeFalse)
-	should.So(t, string(operation.Bytes()), should.Equal, " brwn fx jumps ver lazy d")
+	should.So(t, string(operation.Text), should.Equal, " brwn fx jumps ver lazy d")
 
 	should.So(t, operation.CutAfter("lazy"), should.BeTrue)
 	should.So(t, operation.CutAfter("lazy"), should.BeFalse)
-	should.So(t, string(operation.Bytes()), should.Equal, " brwn fx jumps ver ")
+	should.So(t, string(operation.Text), should.Equal, " brwn fx jumps ver ")
 
 	should.So(t, operation.CutBefore("fx"), should.BeTrue)
 	should.So(t, operation.CutBefore("fx"), should.BeFalse)
-	should.So(t, string(operation.Bytes()), should.Equal, " jumps ver ")
+	should.So(t, string(operation.Text), should.Equal, " jumps ver ")
 
 	should.So(t, operation.CutSuffix("ver "), should.BeTrue)
 	should.So(t, operation.CutSuffix("ver "), should.BeFalse)
-	should.So(t, string(operation.Bytes()), should.Equal, " jumps ")
+	should.So(t, string(operation.Text), should.Equal, " jumps ")
 
 	should.So(t, operation.DiscardNLeft(len(" jumps ")), should.BeTrue)
 	should.So(t, operation.DiscardNLeft(1), should.BeFalse)
 	should.So(t, operation.DiscardNRight(1), should.BeFalse)
-	should.So(t, string(operation.Bytes()), should.Equal, "")
+	should.So(t, string(operation.Text), should.Equal, "")
 }
